64c6f6205790f58c11881f09b1d369ec1ff57917
theshamuel <theshamuel@gmail.com>
2018-05-21 04:28:25 +0300

    refactored User modules

--- api/src/main/java/com/theshamuel/shrturl/user/entity/User.java
+++ api/src/main/java/com/theshamuel/shrturl/user/entity/User.java
@@ -18,6 +18,8 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.Date;
+
 /**
  * The User entity class.
  *
@@ -25,6 +27,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
  */
 @Document(collection = "users")
 public class User extends BaseEntity {
+
 
     @Field("login")
     private String login;
@@ -45,6 +48,29 @@ public class User extends BaseEntity {
     public User() {
     }
 
+    /**
+     * Instantiates a new User.
+     *
+     * @param builder the builder
+     */
+    public User(Builder builder) {
+
+        setId(builder.id);
+
+        setCreatedDate(builder.createdDate);
+
+        setModifyDate(builder.modifyDate);
+
+        setAuthor(builder.author);
+
+        setLogin(builder.login);
+
+        setPassword(builder.password);
+
+        setSalt(builder.salt);
+
+        setRole(builder.role);
+    }
 
     /**
      * Gets login.
@@ -100,12 +126,149 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
+    /**
+     * Gets role.
+     *
+     * @return the role
+     */
     public String getRole() {
         return role;
     }
 
+    /**
+     * Sets role.
+     *
+     * @param role the role
+     */
     public void setRole(String role) {
         this.role = role;
+    }
+
+    /**
+     * Builder builder.
+     *
+     * @return the builder
+     */
+    public static final Builder builder(){
+        return new Builder();
+    }
+
+
+    /**
+     * The type Builder.
+     */
+    public static class Builder{
+        private String id;
+
+        private Date createdDate;
+
+        private Date modifyDate;
+
+        private String author;
+
+        private String login;
+
+        private String password;
+
+        private String salt;
+
+        private String role;
+
+        /**
+         * Instantiates a new User builder.
+         */
+        public Builder() {
+
+        }
+
+        /**
+         * Id user builder.
+         *
+         * @param id the id
+         * @return the user builder
+         */
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        /**
+         * Created date builder.
+         *
+         * @param createdDate the created date
+         * @return the builder
+         */
+        public Builder createdDate(Date createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        /**
+         * Modify date builder.
+         *
+         * @param modifyDate the modify date
+         * @return the builder
+         */
+        public Builder modifyDate(Date modifyDate) {
+            this.modifyDate = modifyDate;
+            return this;
+        }
+
+        /**
+         * Author user builder.
+         *
+         * @param author the author
+         * @return the user builder
+         */
+        public Builder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        /**
+         * Login user builder.
+         *
+         * @param login the login
+         * @return the user builder
+         */
+        public Builder login(String login) {
+            this.login = login;
+            return this;
+        }
+
+
+        /**
+         * Password user builder.
+         *
+         * @param password the password
+         * @return the user builder
+         */
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        /**
+         * Salt user builder.
+         *
+         * @param salt the salt
+         * @return the user builder
+         */
+        public Builder salt(String salt) {
+            this.salt = salt;
+            return this;
+        }
+
+
+        /**
+         * Build user.
+         *
+         * @return the user
+         */
+        public User build() {
+            return new User(this);
+        }
+
     }
 
     @Override
