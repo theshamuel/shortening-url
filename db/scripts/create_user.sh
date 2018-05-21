03c3dec729c7488392a8c3dec799b8cac01dfbef
theshamuel <theshamuel@gmail.com>
2018-05-21 16:54:46 +0300

    deleted old files  and fixed create_user script

--- db/scripts/create_user.sh
+++ db/scripts/create_user.sh
@@ -3,7 +3,6 @@
 if [ "${MONGO_DB}" != "admin" ]; then
     echo "=> Creating a ${MONGO_DB} database user with a password in MongoDB"
     mongo admin -u ${MONGO_ADMIN} -p ${MONGO_ADMIN_PASSWORD} << EOF
-    echo "Using ${MONGO_DB} database"
     use ${MONGO_DB}
     db.createUser({user: '${MONGO_USER}', pwd: '${MONGO_USER_PASSWORD}', roles:[{role:'dbOwner', db:'${MONGO_DB}'}]})
 EOF
